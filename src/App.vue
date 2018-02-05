59d96f23f559966dfe8270f7989210426e62d7fc
luoyuxiang61 <luoyuxiangqop@gmail.com>
2018-02-05 22:40:00 +0800

    0.0.0

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <navi></navi>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import navi from './components/Navi'
+export default {
+  name: 'App',
+  components: {
+    navi
+  }
+}
+</script>
+
+<style>
+#app {
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/components/Navi.vue
@@ -0,0 +1,67 @@
+<template>
+  <header>
+      <div class="head-container">
+          <span class="logo-big"></span>
+          <ul class="navi-list">
+              <li class="navi-list-item"><span>home</span></li>
+              <li class="navi-list-item"><span>videos</span></li>
+              <li class="navi-list-item"><span>donate</span></li>
+          </ul>
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'navi',
+  data () {
+    return {
+      me: 'lyx'
+    }
+  }
+}
+</script>
+
+<style scoped>
+    header {
+        background-color: green;
+    }
+
+    div.head-container {
+        max-width: 1024px;
+        margin: 0 auto;
+        padding-top: 5px;
+    }
+
+    span.logo-big {
+        height: 46px;
+        width: 224px;
+        background-image: url('../assets/logo.png');
+        background-position: -1px -1px;
+        background-repeat: no-repeat;
+        display: inline-block;
+        cursor: pointer;
+    }
+
+    span.logo-big:hover {
+        background-position: -1px -48px;
+    }
+
+    ul.navi-list {
+        list-style: none;
+        display: block;
+        float: right;
+    }
+
+    li.navi-list-item {
+        display: inline-block;
+        line-height: 1.5;
+        padding-top: 8px;
+    }
+
+    li > span:hover {
+        color: white;
+        cursor: pointer;
+    }
+
+</style>
